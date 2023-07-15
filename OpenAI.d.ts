b17c79506915fee7850d38dab9ef5ca0989603ac
DaviSM <davimesquita@gmail.com>
2023-07-15 05:58:23 -0300

    selecao de modelos

--- OpenAI.d.ts
+++ OpenAI.d.ts
@@ -1,32 +1,49 @@
 interface ChatCompletions {
-    id: string;
-    object: string;
-    created: number;
-    model: string;
-    usage: Usage;
-    choices?: (ChoicesEntity)[];
+	id: string;
+	object: string;
+	created: number;
+	model: string;
+	usage: Usage;
+	choices?: (ChoicesEntity)[];
 }
 
 interface Usage {
-    prompt_tokens: number;
-    completion_tokens: number;
-    total_tokens: number;
+	prompt_tokens: number;
+	completion_tokens: number;
+	total_tokens: number;
 }
 
 interface ChoicesEntity {
-    message: MessageChat;
-    finish_reason: string;
-    index: number;
+	message: MessageChat;
+	finish_reason: string;
+	index: number;
 }
 
-type ChatRole = "system" | "user" | "assistant";
+type ChatRole = 'system' | 'user' | 'assistant';
 
 interface MessageChat {
-    role: ChatRole;
-    content: string;
+	role: ChatRole;
+	content: string;
 }
 
 interface ChatCompletionsResponse {
-    messages?: MessageChat[];
-    response: string;
+	messages?: MessageChat[];
+	response: string;
+}
+
+type OpenAIModels = 'gpt-4' | 'gpt-3.5-turbo-16k' | 'gpt-4-32k'
+
+interface ChatCompletionsRequest {
+	model?: OpenAIModels;
+	prompt?: string | string[];
+	suffix?: string;
+	max_tokens?: long;
+	temperature?: number;
+	top_p?: number;
+	n?: number;
+	presence_penalty?: number;
+	frequency_penalty?: number;
+
+	user?: string;
+	messages?: MessageChat[];
 }
--- chatgpt.ts
+++ chatgpt.ts
@@ -1,35 +1,40 @@
-import {fetchWithTimeout, postBearer} from "../util-js/util";
+import { fetchWithTimeout, postBearer } from '../util-js/util';
 
-export const urlCompletions = "https://api.openai.com/v1/chat/completions";
+export const urlCompletions = 'https://api.openai.com/v1/chat/completions';
 
 export function gptslice(conversas: Array<MessageChat>, limit = 16384) {
-    let simpleCount = JSON.stringify(conversas).length;
-    while (simpleCount > (limit)) {
-        conversas.shift();
-        simpleCount = JSON.stringify(conversas).length;
-    }
+	let simpleCount = JSON.stringify(conversas).length;
+	while (simpleCount > (limit)) {
+		conversas.shift();
+		simpleCount = JSON.stringify(conversas).length;
+	}
 }
 
-export async function chat(userId: string, messages: Array<MessageChat>, apikey: string, model = "gpt-3.5-turbo-16k"): Promise<MessageChat | null> {
-    try {
-        let content = {
-            "model": model,
-            "top_p": 0.1,
-            "user": userId,
-            "messages": messages,
-        }
+export async function chat(userId: string, messages: Array<MessageChat>, apikey: string, model = 'gpt-3.5-turbo-16k'): Promise<MessageChat | null> {
+	let content = {
+		'model': model,
+		'top_p': 0.1,
+		'user': userId,
+		'messages': messages,
+	} as ChatCompletionsRequest;
 
-        let response = await fetchWithTimeout(fetch(urlCompletions, postBearer(content, apikey)), 300000);
+	return call(content, apikey);
+}
 
-        if (response !== null) {
-            let data: ChatCompletions = await response.json();
+export async function call(content: ChatCompletionsRequest, apikey: string): Promise<MessageChat | null> {
+	try {
 
-            if (data.choices) {
-                return data.choices[0].message;
-            }
-        }
-    } catch (e) {
-        console.error("chatgpt", e);
-    }
-    return null;
+		let response = await fetchWithTimeout(fetch(urlCompletions, postBearer(content, apikey)), 300000);
+
+		if (response !== null) {
+			let data: ChatCompletions = await response.json();
+
+			if (data.choices) {
+				return data.choices[0].message;
+			}
+		}
+	} catch (e) {
+		console.error('chatgpt', e);
+	}
+	return null;
 }
