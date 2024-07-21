e44ca5e1685133f05d0221abda31367dfe36c4eb
DaviSM <davimesquita@gmail.com>
2024-07-20 23:59:45 -0300

    feat: novo modelo

--- OpenAI.d.ts
+++ OpenAI.d.ts
@@ -42,7 +42,15 @@ interface ChatCompletionsResponse {
     response: string;
 }
 
-type OpenAIModels = 'gpt-4o'|'gpt-4-turbo'|'gpt-3.5-turbo' | 'gpt-4' | 'gpt-3.5-turbo-16k' | 'gpt-4-32k' | 'gpt-4-turbo-preview';
+type OpenAIModels =
+    'gpt-4o-mini'
+    | 'gpt-4o'
+    | 'gpt-4-turbo'
+    | 'gpt-3.5-turbo'
+    | 'gpt-4'
+    | 'gpt-3.5-turbo-16k'
+    | 'gpt-4-32k'
+    | 'gpt-4-turbo-preview';
 
 interface ChatCompletionsRequest {
     model?: OpenAIModels | string;
--- chatgpt.ts
+++ chatgpt.ts
@@ -5,61 +5,61 @@ export const urlTranscr = 'https://api.openai.com/v1/audio/transcriptions';
 export const urlAudio = 'https://api.openai.com/v1/audio/speech';
 
 export function gptslice(conversas: Array<MessageChat>, limit = 16384) {
-	let simpleCount = JSON.stringify(conversas).length;
-	while (simpleCount > (limit)) {
-		conversas.shift();
-		simpleCount = JSON.stringify(conversas).length;
-	}
+    let simpleCount = JSON.stringify(conversas).length;
+    while (simpleCount > (limit)) {
+        conversas.shift();
+        simpleCount = JSON.stringify(conversas).length;
+    }
 }
 
-export async function chat(userId: string, messages: Array<MessageChat>, apikey: string, model = 'gpt-3.5-turbo-16k'): Promise<MessageChat | null> {
-	let content = {
-		'model': model,
-		'top_p': 0.1,
-		'user': userId,
-		'messages': messages,
-	} as ChatCompletionsRequest;
+export async function chat(userId: string, messages: Array<MessageChat>, apikey: string, model = 'gpt-4o-mini'): Promise<MessageChat | null> {
+    let content = {
+        'model': model,
+        'top_p': 0.1,
+        'user': userId,
+        'messages': messages,
+    } as ChatCompletionsRequest;
 
-	return call(content, apikey);
+    return call(content, apikey);
 }
 
 export async function speech(msg: AudioMsg, apikey: string): Promise<Blob> {
 
-	let response = await fetch(urlAudio, postBearer({data:msg, apikey}));
+    let response = await fetch(urlAudio, postBearer({data: msg, apikey}));
 
-	return await response.blob();
+    return await response.blob();
 
 }
 
 export async function call(content: ChatCompletionsRequest, apikey: string, timeout = 300000): Promise<MessageChat | null> {
-	try {
+    try {
 
-		let response = await fetchWithTimeout(fetch(urlCompletions, postBearer({data:content, apikey})), timeout);
+        let response = await fetchWithTimeout(fetch(urlCompletions, postBearer({data: content, apikey})), timeout);
 
-		if (response !== null) {
-			let data: ChatCompletions = await response.json();
+        if (response !== null) {
+            let data: ChatCompletions = await response.json();
 
-			if (data.choices) {
-				return data.choices[0].message;
-			}
-		}
-	} catch (e) {
-		console.error('chatgpt', e);
-	}
-	return null;
+            if (data.choices) {
+                return data.choices[0].message;
+            }
+        }
+    } catch (e) {
+        console.error('chatgpt', e);
+    }
+    return null;
 }
 
 export async function transcribe(file: any, apikey: string): Promise<Transcription> {
 
-	let forma = new FormData();
-	forma.append('file', file, 'audio.webm');
-	forma.append('model', 'whisper-1');
-	return (await fetch(urlTranscr, {
-		method: 'POST',
-		headers: {
-			'Authorization': 'Bearer ' + apikey,
-		},
-		body: forma,
-	})).json();
+    let forma = new FormData();
+    forma.append('file', file, 'audio.webm');
+    forma.append('model', 'whisper-1');
+    return (await fetch(urlTranscr, {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + apikey,
+        },
+        body: forma,
+    })).json();
 
 }
